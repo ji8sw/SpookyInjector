251af1dfb4e74aa3a389c3e400193d49c6718d02
Ji8sw <pakistansacrificer69@gmail.com>
2024-11-19 19:51:40 +1000

    Added Auto Inject Updated README.md

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,22 @@
 ﻿using System.Windows;
 using Microsoft.Win32;
-using System;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Media;
 
 namespace SpookyInjector
 {
     public partial class MainWindow : Window
     {
+        string AutoInjectFile = "";
+        bool AutoInjectEnabled = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            Thread BackgroundThread = new Thread(new ThreadStart(BackgroundLoop));
+            BackgroundThread.IsBackground = true;
+            BackgroundThread.Start();
         }
 
         private void FolderOpen_Click(object sender, RoutedEventArgs e)
@@ -18,8 +24,35 @@ namespace SpookyInjector
             Process.Start("explorer.exe", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Spooky");
         }
 
+        private void ToggleAutoInject_Click(object sender, RoutedEventArgs e)
+        {
+            if (!AutoInjectEnabled)
+            {
+                OpenFileDialog OpenFileDialog = new OpenFileDialog();
+                OpenFileDialog.Filter = "Dynamic Link Libraries (*.dll)|*.dll";
+                OpenFileDialog.DefaultExt = ".dll";
+
+                bool? Result = OpenFileDialog.ShowDialog();
+
+                if (Result == true)
+                {
+                    AutoInjectFile = OpenFileDialog.FileName;
+                    AutoInjectEnabled = true;
+                    ToggleAutoInject.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#262626"));
+                    MessageBox.Show("toggled on");
+                }
+            } else
+            {
+                AutoInjectFile = "";
+                AutoInjectEnabled = false;
+                ToggleAutoInject.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#363636"));
+                MessageBox.Show("toggled off");
+            }
+        }
+
         private void InjectButton_Click(object sender, RoutedEventArgs e)
         {
+            AutoInjectEnabled = false;
             OpenFileDialog OpenFileDialog = new OpenFileDialog();
             OpenFileDialog.Filter = "Dynamic Link Libraries (*.dll)|*.dll";
             OpenFileDialog.DefaultExt = ".dll";
@@ -36,6 +69,26 @@ namespace SpookyInjector
                 {
                     if (!Helper.InjectDLL(PID, Filename)) MessageBox.Show("Failed to inject");
                 } else MessageBox.Show("Failed to find Phasmophobia, is the game running?");
+            }
+        }
+
+        void BackgroundLoop()
+        {
+            while (true)
+            {
+                if (AutoInjectEnabled && AutoInjectFile.Length != 0)
+                {
+                    bool Success = false; int PID = 0;
+                    Helper.GetProcessIDByName(out Success, out PID, "Phasmophobia");
+                    if (Success) Thread.Sleep(5000);
+                    Helper.GetProcessIDByName(out Success, out PID, "Phasmophobia"); // to make sure its still open
+                    if (Success && AutoInjectEnabled)
+                    {
+                        Thread.Sleep(5000);
+                        if (!Helper.InjectDLL(PID, AutoInjectFile)) MessageBox.Show("Failed to auto inject");
+                    }
+                }
+                Thread.Sleep(500);
             }
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# SpookyInjector
+# Spooky Injector
+Spooky Injector is the DLL injector that is paired with the Spooky cheat for Phasmophobia, it is ultra simple.
+
+## Inject
+Once clicked, the inject button will ask you to select a DLL file to inject, once you select one it will find `Phasmophobia.exe` and inject. There is no way to inject a DLL into another process unless you change the source code, it is designed this way.
+
+## Open Spooky Folder
+Once clicked, will open the folder associated with Spooky (`%appdata%/Spooky`), this folder holds configuration files.
+
+## Auto Inject
+Once toggled on, it will ask you for a DLL file and then proceed to wait until Phasmophobia is opened, before injecting the selected DLL (after 5s)
